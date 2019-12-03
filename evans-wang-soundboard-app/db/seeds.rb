24a8f46821114c72854a0040d9a17c284609ab4a
Alexander Riccio <test35965+learn@gmail.com>
2019-12-03 13:12:31 -0500

    wrote some skeleton sound importing code

--- evans-wang-soundboard-app/db/seeds.rb
+++ evans-wang-soundboard-app/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def list_folders
+  base_path = Rails.root
+  puts "Rails root at #{base_path}"
+  repo_path = base_path.dirname
+  puts "outer repo path is #{repo_path}"
+  sounds_repo_path = repo_path.join('sounds_repo')
+  puts "looking for sounds repo at path #{sounds_repo_path}"
+  if !sounds_repo_path.directory?
+    raise Error("can't find sounds_repo directory!")
+  end
+  sounds_repo_path_directory = Dir.new(sounds_repo_path)
+  all_sounds_entries = sounds_repo_path_directory.children
+  all_sounds_folders = all_sounds_entries.filter{|s| File.directory?(sounds_repo_path.join(s))}
+  puts "Sounds directories: #{all_sounds_folders}"
+  all_sounds_folders
+end
+
+def populate
+  folders = list_folders
+end
+
+populate
