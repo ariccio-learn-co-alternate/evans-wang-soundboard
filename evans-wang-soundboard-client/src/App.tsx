92502329c3e8b00e6ce236914977254fd5ef0399
Alexander Riccio <test35965+learn@gmail.com>
2019-12-03 16:35:43 -0500

    sucessfully rendering list of sounds

--- evans-wang-soundboard-client/src/App.tsx
+++ evans-wang-soundboard-client/src/App.tsx
@@ -13,10 +13,12 @@ async function content()  {
   const parsed = await resp.json();
   console.log(parsed);
   debugger;
-  const fileResp = await fetch(`api/sounds/${parsed.sounds[0].id}`, {
-    method: 'GET'
-    })
-  debugger;
+  // const fileResp = await fetch(`api/sounds/${parsed.sounds[0].id}`, {
+  //   method: 'GET'
+  //   })
+  // debugger;
+  // return `api/sounds/${parsed.sounds[0].id}`;
+  return parsed.sounds
 }
 
 interface AppProps {
@@ -24,17 +26,43 @@ interface AppProps {
 }
 
 interface AppState {
-
+  audioSrc?: any;
 }
 
+const defaultState: AppState = {
+  audioSrc : ''
+}
+
+const audioItem = (item?: any) =>
+  <p>
+    {item.name}
+    <audio src={item.id ? `http://localhost:3000/api/sounds/${item.id}` : ""} id={`id-${item.id}-${item.name}`} controls/>
+  </p>
+
+
+const audioItems = (items: Array<any>) => 
+  <>
+  {items.map((item) => {return audioItem(item)})}
+  </>
+
 class _App extends React.Component<AppProps, AppState> {
+  state: AppState = defaultState;
+
+  async componentDidMount() {
+    this.setState({audioSrc: await content()});
+    // debugger;
+  }
+
+
 
   render() {
-    content()
+    // content()
+    debugger;
     return (
       <div className="App">
         <p>{}</p>
-  
+        {/* <audio src={this.state.audioSrc ? `http://localhost:3000/${this.state.audioSrc}` : ""} id="farts" controls/> */}
+        {this.state.audioSrc ? audioItems(this.state.audioSrc) : null}
       </div>
     );
 
